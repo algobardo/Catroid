ec1c49e0f0e18678505e2c792bbfaeeb0197dd37
algobardo <mezzetti@gmail.com>
2014-05-16 12:34:38 +0200

    New sleeper

--- robotium/robotium-solo/src/main/java/com/robotium/solo/Sleeper.java
+++ robotium/robotium-solo/src/main/java/com/robotium/solo/Sleeper.java
@@ -1,6 +1,12 @@
 package com.robotium.solo;
 
+import java.lang.*;
+
 class Sleeper {
+	private static int roboSleepAmount = 0;
+	private static int testSleepAmount = 0;
+	private static int currSec = 0;
+	private static long startTime = -1;
 
 	private final int PAUSE = 500;
 	private final int MINIPAUSE = 300;
@@ -30,6 +36,27 @@ class Sleeper {
 	 */
 
 	public void sleep(int time) {
+		time = (int)(time*0.7);
+
+		if(startTime == -1){
+			startTime = System.currentTimeMillis();
+		}
+		if((currSec * 5000) < roboSleepAmount + testSleepAmount ){
+			currSec++;
+			System.out.println("Current robotium sleep amount is " + roboSleepAmount + " and test sleep amount is " + testSleepAmount + " on a "  + (System.currentTimeMillis()-startTime)  + " running time");
+		}
+		StackTraceElement[] ss = Thread.currentThread().getStackTrace();
+		boolean istestsleep = false;
+		for(StackTraceElement el:ss){
+			if(el.getClassName().contains("Solo") && el.getMethodName().contains("sleep")){
+				testSleepAmount += time;
+				istestsleep = true;
+				break;
+			}
+		}
+		if(!istestsleep)
+			roboSleepAmount += time;
+		
 		try {
 			Thread.sleep(time);
 		} catch (InterruptedException ignored) {}

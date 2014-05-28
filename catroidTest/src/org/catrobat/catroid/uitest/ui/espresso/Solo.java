209c62cd793d20a59f6d7356dca8356b9a95c0a9
algobardo <mezzetti@gmail.com>
2014-05-28 15:18:51 +0200

    RobotiumWithText fix

--- catroidTest/src/org/catrobat/catroid/uitest/ui/espresso/Solo.java
+++ catroidTest/src/org/catrobat/catroid/uitest/ui/espresso/Solo.java
@@ -118,6 +118,10 @@ public class Solo {
             @Override
             public boolean matchesSafely(View view) {
 
+                if(!(view instanceof TextView))
+                    return false;
+
+                
                 Pattern pattern = null;
                 try {
                     pattern = Pattern.compile(text);
